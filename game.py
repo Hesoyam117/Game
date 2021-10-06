5efbad1b62e6d3ea105f639c5c8b22c33de9b22a
Hesoyam117 <81217165+Hesoyam117@users.noreply.github.com>
2021-10-06 10:54:56 +0300

    Add files via upload

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 pygame.init()
 win = pygame.display.set_mode((500, 500))
@@ -15,7 +16,21 @@ pygame.image.load('left_2.png'), pygame.image.load('left_3.png'),
 pygame.image.load('left_4.png'), pygame.image.load('left_5.png'),
 pygame.image.load('left_6.png')]
 
+foeWalkLeft = [pygame.image.load('arab_left_1.png'), pygame.image.load('arab_left_2.png'),
+pygame.image.load('arab_left_3.png'), pygame.image.load('arab_left_4.png'),
+pygame.image.load('arab_left_5.png'), pygame.image.load('arab_left_6.png')]
+
+
+
+arr_r = pygame.image.load('arrow.png')
+arr_l = pygame.image.load('arrow_l.png')
+arab = pygame.image.load('arab.png')
+hp_bar = pygame.image.load('hp_bar.png')
+dead = pygame.image.load('dead.png')
+dead_scr = pygame.image.load('deadscreen.png')
+
 bg = pygame.image.load('bg.jpg')
+bg2 = pygame.image.load('bg_2.jpg')
 playerStand = pygame.image.load('idle.png')
 
 clock = pygame.time.Clock()
@@ -24,15 +39,25 @@ x = 50
 y = 435
 width = 60
 height = 71
-speed = 5
+speed = 6
+hp = 100
+death = 0
+hpcount = 0
+hpcount2 = 0
 
 isJump = False
 jumpCount = 10
+levelCount = 0
 
 left = False
 right = False
 animCount = 0
 lastMove = "right"
+
+foe_x = 400
+foe_y = 435
+foe_speed = 3
+
 
 class arrow():
     def __init__(self, x, y, radius, color, facing):
@@ -44,14 +69,26 @@ class arrow():
         self.vel = 8 * facing
 
     def draw(self, win):
-        pygame.draw.circle(win, self.color, (self.x, self.y),
-        self.radius)
+        if facing == 1:
+            win.blit(arr_r, (self.x, self.y))
+        else:
+            win.blit(arr_l, (self.x, self.y))
 
+
+        #pygame.draw.circle(win, self.color, (self.x, self.y),
+        #self.radius)
 
 
 def drawWindow():
     global animCount
-    win.blit(bg, (0, 0))
+    if levelCount == 0:
+        win.blit(bg, (0, 0))
+    if levelCount == 1:
+        win.blit(bg2, (0, 0))
+
+    win.blit(hp_bar, (20, 20))
+    pygame.draw.rect(win, (255, 0, 0), (23, 23, hp, 30))
+    win.blit(arab, (foe_x, foe_y))
     if animCount + 1 >= 30:
         animCount = 0
     if left:
@@ -64,14 +101,37 @@ def drawWindow():
         win.blit(playerStand, (x, y))
     for bullet in bullets:
         bullet. draw(win)
+    if hp <= 0:
+        death = 1
+        win.blit(dead_scr, (0, 0))
+        win.blit(dead, (250, 250))
+
+
 
 
     pygame.display.update()
 
 run = True
 bullets = []
+arabs = []
 while run:
     clock.tick(30)
+    if ((x + width - 25) <= foe_x):
+        foe_x -= foe_speed
+    elif ((x - width + 25) >= foe_x):
+        foe_x += foe_speed
+    if (((x + width - 25) == foe_x) or ((x - width + 25) == foe_x)):
+        #time.sleep
+        if (hp != 0):
+            hp -= 5
+            #time.sleep(5)
+    #if x > 425:
+        #levelCount += 1
+        #x == 50
+        #win.blit(bg2, (0, 0))
+    #elif (x < 45) and (levelCount > 0):
+        #levelCount -= 1
+        #x == 479
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -84,6 +144,12 @@ while run:
 
     keys = pygame.key.get_pressed()
 
+    if keys[pygame.K_l]:
+        if hp > 0:
+            hp -= 5
+    if keys[pygame.K_o]:
+        if (hp < 100 and death == 0):
+            hp += 5
     if keys[pygame.K_f]:
         if lastMove == "right":
             facing = 1
@@ -114,9 +180,9 @@ while run:
     else:
         if jumpCount >= -10:
             if jumpCount < 0:
-                y += (jumpCount ** 2) / 4
+                y += (jumpCount ** 2) / 2
             else:
-                y -= (jumpCount ** 2) / 4
+                y -= (jumpCount ** 2) / 2
             jumpCount -= 1
 
         else:
