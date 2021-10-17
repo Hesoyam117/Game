c7cee484946152516dda8107e39447d96d3a34cb
Hesoyam117 <81217165+Hesoyam117@users.noreply.github.com>
2021-10-17 12:18:53 +0300

    Add files via upload

--- game.py
+++ game.py
@@ -39,6 +39,19 @@ playerStand = pygame.image.load('idle.png')
 
 clock = pygame.time.Clock()
 
+#Координаты объектов на уровне
+ob_moving_r = False
+ob_moving_l = False
+ob_x = 100
+ob_x2 = 720
+ob_x3 = 990
+ob_x4 = 1500
+ob_x5 = 1750
+
+
+
+#Характеристики игрока
+x_level = 50
 x = 50
 y = 435
 width = 60
@@ -48,19 +61,21 @@ hp = 100
 death = 0
 hpcount = 0
 hpcount2 = 0
-
 isJump = False
 jumpCount = 10
-levelCount = 0
 
+#levelCount = 0
+
+#Движение игрока
 left = False
 right = False
 animCount = 0
 lastMove = "right"
+
+#Характеристики врага
 foe_left = True
 foe_right = False
 foe_animCount = 0
-
 foe_x = 400
 foe_y = 435
 foe_speed = 3
@@ -89,14 +104,25 @@ class arrow():
 def drawWindow():
     global animCount
     global foe_animCount
-    if levelCount == 0:
-        win.blit(bg, (0, 0))
-    if levelCount == 1:
-        win.blit(bg2, (0, 0))
+    win.blit(bg, (0, 0))
+    #pygame.draw.rect(win, (0, 255, 0), (ob_x, ob_y, 50, 30))
+    #if levelCount == 1:
+        #win.blit(bg2, (0, 0))
 
     win.blit(hp_bar, (20, 20))
     pygame.draw.rect(win, (255, 0, 0), (23, 23, hp, 30))
     #win.blit(arab, (foe_x, foe_y))
+    pygame.draw.rect(win, (255, 0, 0), (ob_x, 200, 50, 30))
+    pygame.draw.rect(win, (0, 255, 0), (ob_x2, 350, 80, 40))
+    pygame.draw.rect(win, (0, 0, 255), (ob_x3, 400, 70, 50))
+    pygame.draw.rect(win, (255, 0, 0), (ob_x4, 150, 40, 20))
+    pygame.draw.rect(win, (0, 255, 0), (ob_x5, 300, 100, 20))
+    #if ob_moving_r == True:
+        #ob_x += speed
+    #elif ob_moving_l == True:
+        #ob_x -= speed
+
+
     if animCount + 1 >= 30:
         animCount = 0
     if left:
@@ -144,9 +170,14 @@ while run:
         foe_right = True
         foe_left = False
     if (((x + width - 25) == foe_x) or ((x - width + 25) == foe_x) and y >= foe_y):
-        #time.sleep
         if (hp != 0):
             hp -= 5
+    #ob_x = 100
+    #ob_y = 200
+    #while ob_moving_r == True:
+        #ob_x += speed
+    #while ob_moving_l == True:
+        #ob_x -= speed
             #time.sleep(5)
     #if x > 425:
         #levelCount += 1
@@ -182,16 +213,48 @@ while run:
             bullets.append(arrow(round(x + width // 2), round(y +
             height // 2), 5, (255, 0, 0), facing))
 
-    if keys[pygame.K_a] and x > 10:
-        x -= speed
-        left = True
-        right = False
-        lastMove = "left"
-    elif keys[pygame.K_d] and x < 500 - width - 10:
-        x += speed
-        left = False
-        right = True
-        lastMove = "right"
+    if keys[pygame.K_a] and x > 10 and x_level > 10:
+        if (x_level > 1730) or (x_level < 230):
+            x_level -= speed
+            x -= speed
+            left = True
+            right = False
+            lastMove = "left"
+        elif x_level <= 1730 and x_level >= 230:
+            x_level -= speed
+            left = True
+            right = False
+            lastMove = "left"
+            ob_moving_r = True
+            ob_moving_l = False
+            ob_x += speed
+            ob_x2 += speed
+            ob_x3 += speed
+            ob_x4 += speed
+            ob_x5 += speed
+
+
+
+    elif keys[pygame.K_d] and (x < 500 - width - 10) and (x_level < 2000 - width - 10):
+        if (x_level < 230) or (x_level > 1730):
+            x_level += speed
+            x += speed
+            left = False
+            right = True
+            lastMove = "right"
+        elif x_level >= 230 and x_level <= 1730:
+            x_level += speed
+            left = False
+            right = True
+            lastMove = "right"
+            ob_moving_r = False
+            ob_moving_l = True
+            ob_x -= speed
+            ob_x2 -= speed
+            ob_x3 -= speed
+            ob_x4 -= speed
+            ob_x5 -= speed
+
     else:
         left = False
         right = False
